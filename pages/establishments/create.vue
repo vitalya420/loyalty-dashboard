614d26e25cd2a62082c51930de03a5e77a9f5474
Vitaliy <v.izhakivskiy@gmail.com>
2024-10-28 17:52:14 +0200

    Switch update and timezones

--- pages/establishments/create.vue
+++ pages/establishments/create.vue
@@ -9,7 +9,7 @@
           </div>
           <div class="input-group">
             <label for="desc">Description:</label>
-            <textarea id="desc" rows="3" class="text-input" placeholder="Enter description (optional)"/>
+            <textarea id="desc" rows="3" class="text-input" placeholder="Enter description (optional)"></textarea>
           </div>
           <div class="input-group">
             <label for="">Select Images</label>
@@ -21,7 +21,11 @@
         <form class="form">
           <div class="input-group row" style="padding: 10px 0px;">
             <label for="tz">Timezone:</label>
-            <input type="text">
+            <select id="tz" class="text-input" v-model="selectedTimezone">
+              <option v-for="timezone in timezones" :key="timezone.value" :value="timezone.value">
+                {{ timezone.label }}
+              </option>
+            </select>
           </div>
         </form>
       </Card>
@@ -31,3 +35,40 @@
     </Card>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedTimezone: '',
+      timezones: [
+        { label: 'GMT-12:00', value: 'GMT-12:00' },
+        { label: 'GMT-11:00', value: 'GMT-11:00' },
+        { label: 'GMT-10:00', value: 'GMT-10:00' },
+        { label: 'GMT-09:00', value: 'GMT-09:00' },
+        { label: 'GMT-08:00', value: 'GMT-08:00' },
+        { label: 'GMT-07:00', value: 'GMT-07:00' },
+        { label: 'GMT-06:00', value: 'GMT-06:00' },
+        { label: 'GMT-05:00', value: 'GMT-05:00' },
+        { label: 'GMT-04:00', value: 'GMT-04:00' },
+        { label: 'GMT-03:00', value: 'GMT-03:00' },
+        { label: 'GMT-02:00', value: 'GMT-02:00' },
+        { label: 'GMT-01:00', value: 'GMT-01:00' },
+        { label: 'GMT+00:00', value: 'GMT+00:00' },
+        { label: 'GMT+01:00', value: 'GMT+01:00' },
+        { label: 'GMT+02:00', value: 'GMT+02:00' },
+        { label: 'GMT+03:00', value: 'GMT+03:00' },
+        { label: 'GMT+04:00', value: 'GMT+04:00' },
+        { label: 'GMT+05:00', value: 'GMT+05:00' },
+        { label: 'GMT+06:00', value: 'GMT+06:00' },
+        { label: 'GMT+07:00', value: 'GMT+07:00' },
+        { label: 'GMT+08:00', value: 'GMT+08:00' },
+        { label: 'GMT+09:00', value: 'GMT+09:00' },
+        { label: 'GMT+10:00', value: 'GMT+10:00' },
+        { label: 'GMT+11:00', value: 'GMT+11:00' },
+        { label: 'GMT+12:00', value: 'GMT+12:00' },
+      ]
+    };
+  }
+}
+</script>
